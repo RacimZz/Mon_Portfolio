4e1c9e43f5d016afd32cead9e83389e0747e758e
Racim Zenati <136380109+RacimZz@users.noreply.github.com>
2025-02-06 14:35:25 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,47 @@
+const data = {
+    portfolios: [
+        {
+            techStack: "C#, Python, Unity(Unity3D, XR Toolkit), Rhino 8, Websockets",
+            title: "Modelisation Rhino avec IHM en VR ",
+            githubLink: "https://github.com/RacimZz/Rhino-VR",
+            projectLink: "https://drive.google.com/drive/folders/1dvyUFiwMuhgVZudEV7qf73dHN3fY7AeW?usp=drive_link",
+            imgLink: "images/portfolio/MenuVr.PNG"
+        },
+        {
+            techStack: " Jupyter Notebook, ipykernel, Scikit-learn (SVM, KNN, Random Forest, etc)",
+            title: "Apprentissage machine",
+            githubLink: "https://github.com/RacimZz/Intro_machine_learning",
+            projectLink: "https://drive.google.com/file/d/1BzEEETDONCQAZnf-xHs4wJ5SiW6OYwbE/view?usp=drive_link",
+            imgLink: "images/portfolio/103.png"
+        },
+        {
+            techStack: "Python, NumPy, SciPy, Matplotlib",
+            title: "Transformation et Détection de Contours via Modèles Différentiels",
+            githubLink: "https://github.com/RacimZz/traitement_image",
+            projectLink: "https://drive.google.com/drive/folders/1CvVgse207-Avk4UTIHCDsI7BJjbXzEvu?usp=drive_link",
+            imgLink: "images/portfolio/MAP.jpg"
+        },
+        {
+            techStack: "C, Shell",
+            title: "ProjectCOW",
+            githubLink: "https://github.com/RacimZz/ProjectCOW",
+            projectLink: "#",
+            imgLink: "images/portfolio/203.png"
+        },
+        {
+            techStack: "OCaml",
+            title: "OCaml : Dames chinoises",
+            githubLink: "https://github.com/RacimZz/OCAML_project",
+            projectLink: "https://drive.google.com/file/d/11N2cGvesSMDWV4-cmkNjecEpGCIk_GKj/view?usp=drive_link",
+            imgLink: "images/portfolio/201.png"
+        },
+        {
+            techStack: "Python",
+            title: "Tours de Hanoie",
+            githubLink: "https://github.com/RacimZz/Hanoie_tower",
+            projectLink: "https://drive.google.com/file/d/1QGO5sBPu-g3IbTLPohbZWFjj2jbrtRJ7/view?usp=drive_link",
+            imgLink: "images/portfolio/101.png"
+        }
+    ], 
+    
+};
